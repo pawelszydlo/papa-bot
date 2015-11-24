351c5b316c0b44008fc0a82efecfa789161408b1
Paweł Szydło <pawelszydlo@gmail.com>
2015-11-24 22:08:43 +0100

    Reformat example.

--- example/example.go
+++ example/example.go
@@ -24,32 +24,25 @@ type MyExtension struct {
 	startTime time.Time
 }
 
-/*
-You can overwrite any of the methods:
+// Override any methods you need. For more help please take a look at bot's built in extensions.
 
 // Will be run on bot's init or when extension is registered after bot's init.
-func (ext *MyExtension) Init(bot *papaBot.Bot) error { return nil }
-
-// Will be run whenever an URL is found in the message.
-func (ext *MyExtension) ProcessURL(bot *papaBot.Bot, info *papaBot.UrlInfo, channel, sender, msg string) {}
-
-// Will be run on every public message the bot receives.
-func (ext *MyExtension) ProcessMessage(bot *papaBot.Bot, channel, nick, msg string) {}
-
-// Will be run every 5 minutes. Daily will be set to true once per day.
-func (ext *MyExtension) Tick(bot *papaBot.Bot, daily bool) {}
-
-If you need more help please take a look at bot's built in extensions.
-*/
 func (ext *MyExtension) Init(bot *papaBot.Bot) error {
 	ext.startTime = time.Now()
 	return nil
 }
 
+// Will be run every 5 minutes. Daily will be set to true once per day.
 func (ext *MyExtension) Tick(bot *papaBot.Bot, daily bool) {
 	bot.SendMassNotice(
 		fmt.Sprintf("I have been running for %.0f minutes now.", time.Since(ext.startTime).Minutes()))
 }
+
+// Will be run whenever an URL is found in the message.
+// func (ext *MyExtension) ProcessURL(bot *papaBot.Bot, info *papaBot.UrlInfo, channel, sender, msg string) {}
+
+// Will be run on every public message the bot receives.
+// func (ext *MyExtension) ProcessMessage(bot *papaBot.Bot, channel, nick, msg string) {}
 
 // Entry point
 func main() {
