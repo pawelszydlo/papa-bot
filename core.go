c9970e1662a472d6a857ed33ad6ab2da687ec6b4
Paweł Szydło <pawelszydlo@gmail.com>
2017-04-05 17:07:24 +0200

    Version bump.

--- core.go
+++ core.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	Version        = "0.9.9.3"
+	Version        = "0.9.9.4"
 	Debug          = false // Set to true to crash on runtime errors.
 	MsgLengthLimit = 440   // IRC message length limit.
 )
